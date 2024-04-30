09a88493ca8d03f6d8e4735b2b8c4636518bc3ab
Andres J. Nazzari <andresnazzari@gmail.com>
2024-04-30 08:55:47 -0300

    Add getInstance method to Employee class for creating a singleton instance

--- src/veterinary/person/Employee.java
+++ src/veterinary/person/Employee.java
@@ -1,4 +1,31 @@
 package veterinary.person;
 
 public class Employee extends Person {
+    private static Employee instance = null;
+    private String position;
+
+    public Employee(String name, String position){
+        super(name);
+        this.setPosition(position);
+    }
+
+    public static Employee getInstance (String name, String position) {
+        if (instance == null){
+            instance = new Employee(name, position);
+        }
+        return instance;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+    public String getPosition() {return position;}
+
+    @Override
+    public String toString(){
+        return "Employee information: \n" +
+                "Name: " + this.getName() + "\n" +
+                "Position: " + this.getPosition() + ".";
+    }
+
 }
