55b0f0aef8d9b59150c45bb19b8495451397445a
Andres J. Nazzari <andresnazzari@gmail.com>
2024-04-30 08:55:43 -0300

    Refactor Person class to include name property and constructor

--- src/veterinary/person/Person.java
+++ src/veterinary/person/Person.java
@@ -1,4 +1,15 @@
 package veterinary.person;
 
-public class Person {
-}
+abstract class Person {
+    private String name;
+
+    public Person(String name) {
+        this.setName(name);
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) { this.name = name; }
+
+  }
